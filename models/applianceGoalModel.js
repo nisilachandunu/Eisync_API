c9feae0c14ccbf95cde4528189f77c4c24f7eb9a
Nisila Chandunu <85236657+nisilachandunu@users.noreply.github.com>
2023-11-21 12:54:57 +0530

    Update applianceGoalModel.js with new columns

--- models/applianceGoalModel.js
+++ models/applianceGoalModel.js
@@ -1,13 +1,23 @@
 const { DataTypes } = require("sequelize");
 const { sequelize } = require(".");
 
-module.exports = (sequelize,DataTypes) => {
-const applianceGoal = sequelize.define('applianceGoal', {
- 
-  })
+module.exports = (sequelize, DataTypes) => {
+  const applianceGoal = sequelize.define("applianceGoal", {
+    onHours: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    onTime: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    offTime: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  });
   Goal.belongsToMany(Appliance, { through: applianceGoal });
   Appliance.belongsToMany(Goal, { through: applianceGoal });
 
- 
-  return applianceGoal
-}
+  return applianceGoal;
+};
